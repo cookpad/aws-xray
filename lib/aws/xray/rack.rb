3fcfb203ebe959cf25ee03c0d68d9076898a7ef3
Taiki Ono <taiks.4559@gmail.com>
2017-05-24 00:29:42 +0900

    Rack middleware

---
+++ lib/aws/xray/rack.rb
@@ -0,0 +1,38 @@
+require 'aws/xray/trace_header'
+require 'aws/xray/client'
+require 'aws/xray/context'
+
+module Aws
+  module Xray
+    class Rack
+      TRACE_ENV = 'HTTP_X_AMZN_TRACE_ID'.freeze
+
+      # XXX: excluded_paths, included_paths
+      # XXX: document about client_options
+      def initialize(app, name:, client_options: {})
+        @app = app
+        @name = name
+        @client = Client.new(client_options)
+      end
+
+      TRACE_HEADER = 'X-Amzn-Trace-Id'.freeze
+
+      def call(env)
+        header_value = env[TRACE_ENV]
+        trace_header = if header_value
+                         TraceHeader.build_from_header_value(header_value)
+                       else
+                         TraceHeader.generate
+                       end
+
+        Context.with_new_context(@name, @client, trace_header) do
+          Context.current.base_trace do
+            status, headers, body = @app.call(env)
+            headers[TRACE_HEADER] = trace_header.to_header_value
+            [status, headers, body]
+          end
+        end
+      end
+    end
+  end
+end
