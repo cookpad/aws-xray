390aaff2998de089fe0f945546ca4ffa8453e0bd
Taiki Ono <taiks.4559@gmail.com>
2017-05-24 00:29:24 +0900

    Trace header

---
+++ lib/aws/xray/trace_header.rb
@@ -0,0 +1,49 @@
+require 'aws/xray/header_parser'
+require 'securerandom'
+
+module Aws
+  module Xray
+    class TraceHeader
+      class << self
+        def generate(now = Time.now)
+          new(root: generate_root(now))
+        end
+
+        def build_from_header_value(header_value)
+          h = HeaderParser.parse(header_value)
+          new(root: h.fetch('Root'), sampled: h['Sampled'] != '0', parent: h['Parent'])
+        end
+
+        private
+
+        # XXX: securerandom?
+        def generate_root(now)
+          "1-#{now.to_i.to_s(16)}-#{SecureRandom.hex(12)}"
+        end
+      end
+
+      attr_reader :root, :parent
+
+      def initialize(root:, sampled: true, parent: nil)
+        @root = root
+        @sampled = sampled
+        @parent = parent
+      end
+
+      def to_header_value
+        v = "Root=#{@root};"
+        sampled? ? v << 'Sampled=1' : v << 'Sampled=0'
+        v << ";Parent=#{@parent}" if has_parent?
+        v
+      end
+
+      def sampled?
+        @sampled
+      end
+
+      def has_parent?
+        !!@parent
+      end
+    end
+  end
+end
---
+++ spec/trace_header_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+require 'time'
+
+RSpec.describe Aws::Xray::TraceHeader do
+  context 'with parent and sampled' do
+    let(:v) { 'Root=1-5759e988-bd862e3fe1be46a994272793;Parent=53995c3f42cd8ad8;Sampled=1' }
+
+    it 'created' do
+      r = described_class.build_from_header_value(v)
+      expect(r.root).to eq('1-5759e988-bd862e3fe1be46a994272793')
+      expect(r.parent).to eq('53995c3f42cd8ad8')
+      expect(r.sampled?).to eq(true)
+    end
+
+    it 'dumped' do
+      r = described_class.build_from_header_value(v)
+      expected = 'Root=1-5759e988-bd862e3fe1be46a994272793;Sampled=1;Parent=53995c3f42cd8ad8'
+      expect(r.to_header_value).to eq(expected)
+    end
+  end
+
+  describe '.generate' do
+    it 'returns newly created trace header' do
+      now = Time.parse('2017/01/01 00:00:00Z')
+      r = described_class.generate(now)
+      epoch = r.root.scan(/\A1-([0-9A-Fa-f]+)-/).first.first
+      expect(epoch.to_i(16)).to eq(now.to_i)
+    end
+  end
+end
