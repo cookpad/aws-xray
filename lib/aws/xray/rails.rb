6b510723f0dd116389caa81744440ee0c5dab1a8
Taiki Ono <taiks.4559@gmail.com>
2017-05-29 14:21:32 +0900

    Don't use canonical name for non-production env
    Instead, append -#{env_name} suffix to name.

--- lib/aws/xray/rails.rb
+++ lib/aws/xray/rails.rb
@@ -4,7 +4,15 @@ module Aws
   module Xray
     class Railtie < ::Rails::Railtie
       initializer 'aws-xray.set_name' do |app|
-        Aws::Xray.config.name = app.class.parent_name.underscore.gsub(/(\/|_)/, '-')
+        unless Aws::Xray.config.name
+          app_name = app.class.parent_name.underscore.gsub(/(\/|_)/, '-')
+
+          if Rails.env.production?
+            Aws::Xray.config.name = app_name
+          else
+            Aws::Xray.config.name = "#{app_name}-#{Rails.env}"
+          end
+        end
       end
 
       initializer 'aws-xray.rack_middleware' do |app|
