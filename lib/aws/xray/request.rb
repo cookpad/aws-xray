7a39175ac3d5c123aba9ad077e31168b6252bc95
Taiki Ono <taiks.4559@gmail.com>
2017-06-06 18:08:05 +0900

    Drop query string and fragment part of uri

--- lib/aws/xray/request.rb
+++ lib/aws/xray/request.rb
@@ -1,10 +1,12 @@
+require 'uri'
+
 module Aws
   module Xray
     attrs = [:method, :url, :user_agent, :client_ip, :x_forwarded_for, :traced]
     class Request < Struct.new(*attrs)
       class << self
         def build(method:, url:, user_agent: nil, client_ip: nil, x_forwarded_for: nil, traced: false)
-          new(encode(method), encode(url), encode(user_agent), encode(client_ip), x_forwarded_for, traced)
+          new(encode(method), drop_params(encode(url)), encode(user_agent), encode(client_ip), x_forwarded_for, traced)
         end
 
         def build_from_rack_env(env)
@@ -40,6 +42,15 @@ module Aws
           else
             str.to_s.encode(__ENCODING__, invalid: :replace, undef: :replace)
           end
+        end
+
+        def drop_params(str)
+          uri = URI.parse(str)
+          uri.query = nil
+          uri.fragment = nil
+          uri.to_s
+        rescue URI::InvalidURIError
+          str
         end
       end
     end
