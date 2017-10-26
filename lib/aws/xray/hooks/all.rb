ac95e2db9b49b80bbba399c685ac64574d0e0ec9
Taiki Ono <taiks.4559@gmail.com>
2017-10-26 20:13:54 +0900

    Don't require AR hook in "all" hook

--- lib/aws/xray/hooks/all.rb
+++ lib/aws/xray/hooks/all.rb
@@ -1,4 +1,4 @@
+# Don't require activerecord hook because the hook may produce lots of trace records.
 require 'aws/xray'
-require 'aws/xray/hooks/active_record'
 require 'aws/xray/hooks/net_http'
 require 'aws/xray/hooks/rsolr'
